988450a8ed2b8af1423bc27f2738f64ea7a2c4da
JayPark7821 <pjhyun1990@gmail.com>
2022-08-14 23:48:31 +0900

    전략패턴 예제 2

--- src/test/java/hello/advanced/trace/strategy/ContextV1Test.java
+++ src/test/java/hello/advanced/trace/strategy/ContextV1Test.java
@@ -3,6 +3,7 @@ package hello.advanced.trace.strategy;
 import org.junit.jupiter.api.Test;
 
 import hello.advanced.trace.strategy.code.strategy.ContextV1;
+import hello.advanced.trace.strategy.code.strategy.Strategy;
 import hello.advanced.trace.strategy.code.strategy.StrategyLogic1;
 import hello.advanced.trace.strategy.code.strategy.StrategyLogic2;
 import lombok.extern.slf4j.Slf4j;
@@ -50,6 +51,65 @@ public class ContextV1Test {
 		ContextV1 contextV2 = new ContextV1(strategyLogic2);
 		contextV2.execute();
 
+	}
+	
+	@Test
+	void strategyv2() throws Exception{
+		Strategy strategyLogic1 = new Strategy() {
+			@Override
+			public void call() {
+				log.info("비즈니스 로직1 실행");
+
+			}
+		};
+
+		ContextV1 contextV1 = new ContextV1(strategyLogic1);
+		contextV1.execute();
+
+		Strategy strategyLogic2 = new Strategy() {
+			@Override
+			public void call() {
+				log.info("비즈니스 로직2 실행");
+
+			}
+		};
+
+		ContextV1 contextV2 = new ContextV1(strategyLogic2);
+		contextV2.execute();
+
+	}
+
+	@Test
+	void strategyv3() throws Exception{
+
+		ContextV1 contextV1 = new ContextV1( new Strategy() {
+			@Override
+			public void call() {
+				log.info("비즈니스 로직1 실행");
+
+			}
+		});
+		contextV1.execute();
+
+		ContextV1 contextV2 = new ContextV1(new Strategy() {
+			@Override
+			public void call() {
+				log.info("비즈니스 로직2 실행");
+
+			}
+		});
+		contextV2.execute();
+	}
+
+	@Test
+	void strategyv4() throws Exception{
+
+
+		ContextV1 contextV1 = new ContextV1(() -> log.info("비즈니스 로직1 실행"));
+		contextV1.execute();
+
+		ContextV1 contextV2 = new ContextV1(() -> log.info("비즈니스 로직2 실행"));
+		contextV2.execute();
 	}
 
 }
